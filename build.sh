623defabc2c28903e84cb86a9e813a7b23f1b697
Gavin Bisesi <gavin.bisesi@47deg.com>
2022-02-11 16:09:11 -0500

    build xpi using web-ext

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-files="css images lib manifest.json popup.html"
-zip -r tab-manager-firefox.xpi $files -x "*.DS_Store" -x "__MACOSX"
+if [[ ! -e "$(npm bin)/web-ext" ]]; then
+  echo "web-ext not found; installing"
+  npm install web-ext
+fi
+webext="$(npm bin)/web-ext"
 
+# files="css images lib manifest.json popup.html"
+# zip -r tab-manager-firefox.xpi $files -x "*.DS_Store" -x "__MACOSX"
+
+"$webext" build
